00833faf1c5874c1704ea28de08d550adf26d427
roxyrong <rongxinyun2010@gmail.com>
2018-11-30 02:14:45 -0600

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import bg from './assets/background.jpg';
+import logo from './assets/white_logo.png';
+import * as styles from "./styles";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={styles.appStyle}>
+        <div className="container">
+          <nav className="navbar fixed-top navbar-light navbar-expand-md navbar-burger">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/"><img src={logo} style={styles.logoStyle} alt="logo" /></a>
+              <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item"><a className="nav-link" style={styles.navStyle} href="/">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" style={styles.navStyle} href="/">Event</a></li>
+                    <li className="nav-item"><a className="nav-link" style={styles.navStyle} href="/">Membership</a></li>
+                    <li className="nav-item"><a className="nav-link" style={styles.navStyle} href="/">Login</a></li>
+                  </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <img src={bg} style={styles.backgroundPic} alt="background-pic" />
+        
       </div>
     );
   }
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+export const appStyle = {
+    "fontFamily": "Helvetica",
+}
+
+export const backgroundPic = {
+    "width": "100%",
+    "filter": "brightness(60%)",
+}
+
+export const logoStyle = {
+    "width": "50px",
+}
+
+export const navStyle = {
+    "color": "white",
+    "fontSize": "20px"
+}
