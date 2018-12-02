744a5d3b288212f19bb5e62da148adf114955818
roxyrong <rongxinyun2010@gmail.com>
2018-12-02 10:24:51 -0600

    change style file

--- src/styles.js
+++ src/styles.js
@@ -1,59 +1,59 @@
 export const appStyle = {
-    "fontFamily": "Helvetica",
+    fontFamily: "Helvetica",
 }
 
 export const backgroundPic = {
-    "width": "100%",
-    "filter": "brightness(60%)",
-    "zIndex": "-1",
-    "position": "relative",
+    width: "100%",
+    filter: "brightness(60%)",
+    zIndex: "-1",
+    position: "relative",
 }
 
 export const whiteLogo = {
-    "width": "50px",
+    width: "50px",
 }
 
 export const navStyle = {
-    "color": "white",
-    "fontSize": "20px"
+    color: "white",
+    fontSize: "20px"
 }
 
 export const landingPageTitle = {
-    "textAlign": "center",
-    "marginTop": "-55%",
-    "color": "#1DCB8B",
-    "textShadow": "2px 2px #000000",
-    "letterSpacing": "25px"
+    textAlign: "center",
+    marginTop: "-55%",
+    color: "#1DCB8B",
+    textShadow: "2px 2px #000000",
+    letterSpacing: "25px"
 }
 
 export const tagline = {
-    "marginTop": "2%",
-    "textAlign": "center",
-    "color": "white",
-    "fontSize": "40px",
-    "letterSpacing": "5px"
+    marginTop: "2%",
+    textAlign: "center",
+    color: "white",
+    fontSize: "40px",
+    letterSpacing: "5px"
 }
 
 export const aboutUs = {
-    "marginTop" : "40%",
+    marginTop : "40%",
 
 }
 
 export const aboutUsHeader = {
-    "marginTop": "50px",
+    marginTop: "50px",
 }
 
 export const aboutUsText = {
-    "fontWeight": "200"
+    fontWeight: "200"
 }
 
 export const blackLogo = {
-    "width": "90%",
+    width: "90%",
 }
 
 export const introVideo = {
-    "marginTop": "100px",
-    "marginBottom": "100px"
+    marginTop: "100px",
+    marginBottom: "100px"
 }
 
 export const eventLeft = {
@@ -62,82 +62,82 @@ export const eventLeft = {
 }
 
 export const eventMiddle = {
-    "backgroundColor": "black",
-    "height": "600px",
+    backgroundColor: "black",
+    height: "600px",
 }
 
 export const eventRight = {
-    "backgroundColor": "#B8ECD6",
-    "height": "600px",
+    backgroundColor: "#B8ECD6",
+    height : "600px",
 }
 
 export const eventTitle = {
-    "marginTop": "100px",
+    marginTop: "100px",
 }
 
 export const eventHeader = {
-    "color": "#1DCB8B",
-    "fontWeight": "bold",
-    "marginTop": "50px",
-    "paddingLeft": "30%",
-    "paddingRight": "30%",
+    color: "#1DCB8B",
+    fontWeight: "bold",
+    marginTop: "50px",
+    paddingLeft: "30%",
+    paddingRight: "30%",
 }
 
 export const eventContent = {
-    "marginTop": "10px",
-    "paddingLeft": "30%",
-    "paddingRight": "30%",
+    marginTop: "10px",
+    paddingLeft: "30%",
+    paddingRight: "30%",
 }
 
 export const eventButton = {
-    "width": "40%",
-    "marginLeft": "30%",
-    "marginRight": "30%",
-    "backgroundColor": "#1DCB8B",
-    "borderColor": "#1DCB8B",
-    "marginTop": "50px"
+    width: "40%",
+    marginLeft: "30%",
+    marginRight: "30%",
+    backgroundColor: "#1DCB8B",
+    borderColor: "#1DCB8B",
+    marginTop: "50px"
 }
 
 export const eventLineBreak = {
-    "backgroundColor": "white",
-    "width": "50px",
-    "height": "10px",
-    "margin": "100px auto 0 auto"
+    backgroundColor: "white",
+    width: "50px",
+    height: "10px",
+    margin: "100px auto 0 auto"
 }
 
 export const eventMiddleText1 = {
-    "color": "#1DCB8B", 
-    "textAlign": "center",
-    "fontWeight": "bold",
-    "marginTop": "100px"
+    color: "#1DCB8B", 
+    textAlign: "center",
+    fontWeight: "bold",
+    marginTop: "100px"
 }
 
 export const eventMiddleText2 = {
-    "color": "white",
-    "textAlign": "center",
-    "fontWeight": "bold",
-    "marginTop": "50px"
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+    marginTop: "50px"
 }
 
 export const eventButtonMiddle = {
-    "width": "40%",
-    "marginLeft": "30%",
-    "marginRight": "30%",
-    "color": "white",
-    "backgroundColor": "black",
-    "marginTop": "20px"
+    width: "40%",
+    marginLeft: "30%",
+    marginRight: "30%",
+    color: "white",
+    backgroundColor: "black",
+    marginTop: "20px"
 }
 
 export const teamPic = {
-    "width": "100%",
-    "padding": "5% 20% 0 20%",
+    width: "100%",
+    padding: "5% 20% 0 20%",
 }
 
 export const teamHeader = {
-    "marginTop": "100px",
+    marginTop: "100px",
 }
 
 export const teamText = {
-    "padding": "2% 20%", 
-    "fontWeight": "200"
+    padding: "2% 20%", 
+    fontWeight: "200"
 }
