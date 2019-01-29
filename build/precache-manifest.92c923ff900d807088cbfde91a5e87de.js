121f9b6f6022169f4d17098b62ee4c90af39b84c
roxyrong <rongxinyun2010@gmail.com>
2019-01-29 01:02:25 -0600

    add build folder

---
+++ build/precache-manifest.92c923ff900d807088cbfde91a5e87de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5bd8eea5c02567c2a5350f0f0758179",
+    "url": "/static/media/intro_video_screenshot.a5bd8eea.png"
+  },
+  {
+    "revision": "ee2dce749ac7058f2b86",
+    "url": "/static/js/main.ee2dce74.chunk.js"
+  },
+  {
+    "revision": "dc4593f7b9214d74187c58c83b76c559",
+    "url": "/static/media/membership_bg.dc4593f7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c6fe84beefd1987c2fae235ec052937",
+    "url": "/static/media/white_logo.1c6fe84b.png"
+  },
+  {
+    "revision": "261c07d66110b3ba3c9d1d737e507722",
+    "url": "/static/media/background.261c07d6.jpg"
+  },
+  {
+    "revision": "521a3426e7de771935bc38db4bc82b94",
+    "url": "/static/media/black_logo.521a3426.jpg"
+  },
+  {
+    "revision": "d586706c27e37b866653635ab8c968fb",
+    "url": "/static/media/signin-image.d586706c.jpg"
+  },
+  {
+    "revision": "9a90ea84a2a457e11f8275ce41ac592c",
+    "url": "/static/media/Build312_promo.9a90ea84.mp4"
+  },
+  {
+    "revision": "910c0460478ce0c7728efebd85c21994",
+    "url": "/static/media/team_pic.910c0460.jpg"
+  },
+  {
+    "revision": "75eb7c4e5d58a0a3f0d387b9bc44e080",
+    "url": "/static/media/signup-image.75eb7c4e.jpg"
+  },
+  {
+    "revision": "d48864dc8b117485b103",
+    "url": "/static/js/1.d48864dc.chunk.js"
+  },
+  {
+    "revision": "d48864dc8b117485b103",
+    "url": "/static/css/1.4450738d.chunk.css"
+  },
+  {
+    "revision": "87a8279da384af51a0bb112fd2124de4",
+    "url": "/index.html"
+  }
+];
